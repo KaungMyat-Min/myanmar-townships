54e1b54e51d53a469a937b1a5e0b252515e8a7e4
Kaung Myat Min <kaung.mm@blueplanet.com.mm>
2020-01-03 10:04:57 +0630

    finsh adding config file

--- packages/kaungmyat-min/myanmar-townships/src/MyanmarTownshipsProvider.php
+++ packages/kaungmyat-min/myanmar-townships/src/MyanmarTownshipsProvider.php
@@ -23,7 +23,13 @@ class MyanmarTownshipsProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
 
+        $this->publishes([
+            __DIR__ . '/config/myanmar-township.php' => config_path('myanmar-townships.php'),
+        ]);
+
+        $this->mergeConfigFrom(__DIR__ . '/config/myanmar-townships.php', 'myanmar-township');
+        $this->mergeConfigFrom(__DIR__ . '/config/township.php', 'township');
     }
 }
---
+++ packages/kaungmyat-min/myanmar-townships/src/config/myanmar-townships.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'search_by_unicode' => true,
+    'search_by_zawgyi' => true,
+    'search_by_english' => true,
+];
